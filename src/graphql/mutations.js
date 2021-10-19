e6f8c39a84de5070450c7f9987c71f9842a72f91
Adrian Silva <silvadrian3@gmail.com>
2021-10-19 15:20:08 +0800

    add graphQL

---
+++ src/graphql/mutations.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createProducts = /* GraphQL */ `
+  mutation CreateProducts(
+    $input: CreateProductsInput!
+    $condition: ModelProductsConditionInput
+  ) {
+    createProducts(input: $input, condition: $condition) {
+      id
+      name
+      description
+      selling_price
+    }
+  }
+`;
+export const updateProducts = /* GraphQL */ `
+  mutation UpdateProducts(
+    $input: UpdateProductsInput!
+    $condition: ModelProductsConditionInput
+  ) {
+    updateProducts(input: $input, condition: $condition) {
+      id
+      name
+      description
+      selling_price
+    }
+  }
+`;
+export const deleteProducts = /* GraphQL */ `
+  mutation DeleteProducts(
+    $input: DeleteProductsInput!
+    $condition: ModelProductsConditionInput
+  ) {
+    deleteProducts(input: $input, condition: $condition) {
+      id
+      name
+      description
+      selling_price
+    }
+  }
+`;
